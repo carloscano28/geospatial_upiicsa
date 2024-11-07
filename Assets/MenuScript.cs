71e34e56f3b81fcdb1367fa65d0cb2b28efd9f42
Carlos R. <c.r.cano@outlook.com>
2024-11-06 19:45:59 -0600

    Inicio
    Creación de proyecto Geospatial Upiicsa

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void BotonStart()
+    {
+        SceneManager.LoadScene("HelloCardboard", LoadSceneMode.Single);
+    }
+
+    // Update is called once per frame
+    public void BotonSalir()
+    {
+        Debug.Log("Salimos de la app");
+        Application.Quit();
+    }
+}
